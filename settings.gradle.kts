eabb997530c53e05b388375365e62f4489a21011
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-10-11 16:07:25 +0600

    initial version

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Student Management Client"
+include(":app")
+include(":studentmanagementadmin")
